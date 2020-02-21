cdb368d3866a063e3d790494b7c831c1a3a4a21a
Abdus Sattar Bhuiyan <sattar.kuet@gamil.com>
2020-02-22 02:34:25 +0600

    first commit

---
+++ variable/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace variable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Byte a;
+            Console.WriteLine(Byte.MinValue);
+            Console.WriteLine(" to ");
+            Console.WriteLine(Byte.MaxValue);
+        }
+    }
+}
